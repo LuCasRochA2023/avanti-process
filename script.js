661fb388ffa8775b0c5fbbcb64bb44c95221c667
lucas <mrlukas2012@hotmail.com>
2025-03-30 20:35:34 -0400

    adicionado todos componentes finalizando o layout desktop

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function carrgaComponente(id, file) {
+function loadComponent(id, file) {
     fetch(file)
         .then(response => response.text())
         .then(data => {
@@ -8,7 +8,17 @@ function carrgaComponente(id, file) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    carrgaComponente("header", "src/components/header/html/header.html");
-    carrgaComponente("banner", "src/components/banner/html/banner.html");
-    carrgaComponente("cards", "src/components/cards/html/cards.html");
+    loadComponent("header", "src/components/header/html/header.html");
+    loadComponent("banner", "src/components/banner/html/banner.html");
+    loadComponent("cards", "src/components/cards/html/cards.html");
+    loadComponent("cards-2", "src/components/cards/html/cards.html");
+    loadComponent("mugFirist", "src/components/mugFirist/html/mugFirist.html");
+    loadComponent("mugFirist-2", "src/components/mugFirist/html/mugFirist.html");
+    loadComponent("mugSecond", "src/components/mugSecond/html/mugSecond.html");
+    loadComponent("mugSecond-2", "src/components/mugSecond/html/mugSecond.html");
+    loadComponent("second-banner", "src/components/secondBanner/html/secondBanner.html");
+    loadComponent("newsletter", "src/components/newsletter/html/newsletter.html");
+    loadComponent("footer", "src/components/footer/html/footer.html");
+
+    
 });
